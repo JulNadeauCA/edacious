8ad4ac778187e1263e7f52f291ec675bb1700b5d
Julien Nadeau Carriere <vedge@hypertriton.com>
2008-05-05 02:17:45 +0000

    port editor tool
    git-svn-id: file:///home/dev/edacious/trunk@146 2ffb977e-9902-0410-a00a-94b404125a2c

---
+++ schem/port_tool.c
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2008 Hypertriton, Inc. <http://hypertriton.com/>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * Port edition tool.
+ */
+
+#include <eda.h>
+#include <agar/gui/primitive.h>
+#include <agar/core/limits.h>
+
+typedef struct es_schem_select_tool {
+	VG_Tool _inherit;
+} ES_SchemPortTool;
+
+static int
+MouseButtonDown(void *p, VG_Vector vPos, int button)
+{
+	ES_SchemPortTool *t = p;
+	ES_Schem *scm = VGTOOL(t)->p;
+	VG_View *vv = VGTOOL(t)->vgv;
+	VG_Point *vp;
+	ES_SchemPort *sp;
+
+	if (button != SDL_BUTTON_LEFT) {
+		return (0);
+	}
+	if ((vp = VG_SchemFindPoint(vv, vPos, NULL)) != NULL) {
+		sp = ES_SchemPortNew(vp, vp);
+	} else {
+		VG_Status(vv, _("Select a point to create a port"));
+	}
+	return (1);
+}
+
+static int
+MouseMotion(void *p, VG_Vector vPos, VG_Vector vRel, int buttons)
+{
+	ES_SchemPortTool *t = p;
+	ES_Schem *scm = VGTOOL(t)->p;
+	VG_View *vv = VGTOOL(t)->vgv;
+	VG_Point *vp;
+
+	if ((vp = VG_SchemHighlightNearestPoint(vv, scm, vPos, NULL)) != NULL) {
+		VG_Status(vv, _("Create port on Point%u"), VGNODE(vp)->handle);
+	} else {
+		VG_Status(vv, _("Select a Point"));
+	}
+	return (0);
+}
+
+static int
+KeyDown(void *p, int ksym, int kmod, int unicode)
+{
+	ES_SchemPortTool *t = p;
+	VG_View *vv = VGTOOL(t)->vgv;
+	VG_Node *vn;
+	Uint nDel = 0;
+
+	if (ksym == SDLK_DELETE || ksym == SDLK_BACKSPACE) {
+del:
+		TAILQ_FOREACH(vn, &vv->vg->nodes, list) {
+			if (!VG_NodeIsClass(vn, "SchemPort") ||
+			    !(vn->flags & VG_NODE_SELECTED)) {
+				continue;
+			}
+			if (VG_Delete(vn) == -1) {
+				vn->flags &= ~(VG_NODE_SELECTED);
+				VG_Status(vv, "%s", AG_GetError());
+			} else {
+				nDel++;
+			}
+			goto del;
+		}
+		VG_Status(vv, _("Deleted %u ports"), nDel);
+		return (1);
+	}
+	return (0);
+}
+
+VG_ToolOps esSchemPortTool = {
+	N_("Port Editor"),
+	N_("Create connection points for the schematic."),
+	&esIconPortEditor,
+	sizeof(ES_SchemPortTool),
+	VG_NOSNAP,
+	NULL,
+	NULL,			/* destroy */
+	NULL,			/* edit */
+	NULL,			/* predraw */
+	NULL,			/* postdraw */
+	MouseMotion,
+	MouseButtonDown,
+	NULL,			/* mousebuttonup */
+	KeyDown,
+	NULL			/* keyup */
+};
