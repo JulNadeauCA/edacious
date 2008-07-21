cd84827f23411e0bc8352e4a937934499a03360a
Antoine Levitt <smeuuh@gmail.com>
2008-07-21 16:20:45 +0000

    inline stamp
    git-svn-id: file:///home/dev/edacious/trunk@347 2ffb977e-9902-0410-a00a-94b404125a2c

--- core/stamp.h
+++ core/stamp.h
@@ -10,33 +10,115 @@
  * passes the StampData and the values to write each time it wants to stamp.
  */
 
-__BEGIN_DECLS
 
 typedef M_Real *StampData[];
 
+/* Dummy variable to write everything related to ground to */
+extern M_Real dummy;
+
+
+/* Macros to simplify function bodies */
+#define N SIM(dc)->ckt->n
+
+#define GetElemG(k, l) (((k) == 0 || (l) == 0) ? &dummy : M_GetElement(dc->A, (k), (l)))
+#define GetElemB(k, l) GetElemG(k, N+l)
+#define GetElemC(k, l) GetElemG(N+k, l)
+#define GetElemD(k, l) GetElemG(N+k, N+l)
+
+#define GetElemI(k) ((k) == 0 ? &dummy : M_VecGetElement(dc->z, (k)))
+#define GetElemV(k) GetElemI(k+N)
+
+
+
 /* Conductance */
 #define STAMP_CONDUCTANCE_SIZE 4
-void InitStampConductance(Uint k, Uint l, StampData s, ES_SimDC *dc);
-void StampConductance(M_Real g, StampData s);
+static void __inline__
+InitStampConductance(Uint k, Uint l, StampData s, ES_SimDC *dc)
+{
+	s[0] = GetElemG(k, k);
+	s[1] = GetElemG(k, l);
+	s[2] = GetElemG(l, k);
+	s[3] = GetElemG(l, l);
+}
+static void __inline__
+StampConductance(M_Real g, StampData s)
+{
+	*s[0] += g;
+	*s[1] -= g;
+	*s[2] -= g;
+	*s[3] += g;
+}
 
 /* Current source */
 #define STAMP_CURRENT_SOURCE_SIZE 2
-void InitStampCurrentSource(Uint k, Uint l, StampData s, ES_SimDC *dc);
-void StampCurrentSource(M_Real I, StampData s);
+static void __inline__
+InitStampCurrentSource(Uint k, Uint l, StampData s, ES_SimDC *dc)
+{
+	s[0] = GetElemI(k);
+	s[1] = GetElemI(l);
+}
+static void __inline__
+StampCurrentSource(M_Real I, StampData s)
+{
+	*s[0] += I;
+	*s[1] -= I;
+}
 
 /* Voltage controlled current source */
 #define STAMP_VCCS_SIZE 4
-void InitStampVCCS(Uint k, Uint l, Uint p, Uint q, StampData s, ES_SimDC *dc);
-void StampVCCS(M_Real g, StampData s);
+static void __inline__
+InitStampVCCS(Uint k, Uint l, Uint p, Uint q, StampData s, ES_SimDC *dc)
+{
+	s[0] = GetElemG(p, k);
+	s[1] = GetElemG(p, l);
+	s[2] = GetElemG(q, k);
+	s[3] = GetElemG(q, l);
+}
+static void __inline__
+StampVCCS(M_Real g, StampData s)
+{
+	*s[0] += g;
+	*s[1] -= g;
+	*s[2] -= g;
+	*s[3] += g;
+}
 
 /* Independant voltage source */
 #define STAMP_VOLTAGE_SOURCE_SIZE 5
-void InitStampVoltageSource(Uint k, Uint l, Uint v, StampData s, ES_SimDC *dc);
-void StampVoltageSource(M_Real voltage, StampData s);
+static void __inline__
+InitStampVoltageSource(Uint k, Uint l, Uint v, StampData s, ES_SimDC *dc)
+{
+	s[0] = GetElemB(k, v);
+	s[1] = GetElemC(v, k);
+	s[2] = GetElemB(l, v);
+	s[3] = GetElemC(v, l);
+	s[4] = GetElemV(v);
+}
+static void __inline__ 
+StampVoltageSource(M_Real voltage, StampData s)
+{
+	*s[0] = 1.0;
+	*s[1] = 1.0;
+	*s[2] = -1.0;
+	*s[3] = -1.0;
+	*s[4] = voltage;
+}
 
 /* Thevenin model */
 #define STAMP_THEVENIN_SIZE 6
-void InitStampThevenin(Uint k, Uint l, Uint v, StampData s, ES_SimDC *dc);
-void StampThevenin(M_Real voltage, M_Real resistance, StampData s);
+static void __inline__
+InitStampThevenin(Uint k, Uint l, Uint v, StampData s, ES_SimDC *dc)
+{
+	InitStampVoltageSource(k, l, v, s, dc);
+	s[5] = GetElemD(v, v);
+}
+static void __inline__
+StampThevenin(M_Real voltage, M_Real resistance, StampData s)
+{
+	StampVoltageSource(voltage, s);
+	*s[5] = -resistance;
+}
 
-__END_DECLS
+
+
+#undef N
